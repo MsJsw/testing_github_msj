6a90b263302297d91670f40dcb856bff65bc40aa
MsJsw <matej.smolic@endava.com>
2024-01-01 07:33:07 +0100

    uupdate

--- env_main.py
+++ env_main.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
-api_key = os.getenv("OPEN_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 print(api_key)
